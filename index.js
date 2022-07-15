4f0a2b0beaedc592981143137b21518db09a0c16
Vitor <73979580+Blackklegend@users.noreply.github.com>
2022-07-15 04:46:26 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const rotas = require('./rotas');
 
 app.use(express.json());
-app.use(rotas); 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
